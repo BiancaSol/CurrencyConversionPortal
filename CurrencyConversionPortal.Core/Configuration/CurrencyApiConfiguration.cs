6534e00b551843ab70471b59ad38035309c65044
BiancaSolomon <Bianca.Solomon@visma.com>
2025-07-28 19:58:35 +0300

    Replace HttpClient with IHttpClientFactory and add configuration for external api

---
+++ CurrencyConversionPortal.Core/Configuration/CurrencyApiConfiguration.cs
@@ -0,0 +1,8 @@
+namespace CurrencyConversionPortal.Core.Configuration
+{
+    public class CurrencyApiConfiguration
+    {
+        public const string SectionName = "CurrencyApi";
+        public const string BaseUrlProperty = "BaseUrl";
+    }
+}
